9e8f980056389eb9e84ebd4b55d5bf5006f1dd3c
Daniel Lemes <inventarartcomunicacao@hotmail.com>
2020-07-28 16:00:27 -0300

    exercícios da semana3-aula10

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,33 @@
+/*let age = prompt("qual é sua idade?")
+let friendAge = prompt("idade do melhor amigo?")
+let difference = Number(age) - Number(friendAge)
+let resposta = (age) < (friendAge)
+console.log("Sua idade é maior do que a do seu melhor amigo?")
+console.log(resposta)
+console.log("A diferença de idades é:", difference)*/
+
+/*let number = prompt("insira um número PAR")
+let converts = Number(number)
+number = number%2
+console.log(number)*/
+/* toda vez que coloco um número par, dá resto 0, independente do número par. Se eu coloco número ímpar, dá resto 1, independente do número ímpar*/
+
+
+/* let listaDeTarefas= []
+const tarefa1 = prompt("fale uma tarefa que você realiza durante o dia")
+const tarefa2 = prompt("fale uma tarefa que você realiza durante o dia")
+const tarefa3 = prompt("fale uma tarefa que você realiza durante o dia")
+listaDeTarefas.push(tarefa1)
+listaDeTarefas.push(tarefa2)
+listaDeTarefas.push(tarefa3)
+console.log(listaDeTarefas)
+const indiceDeTarefas = prompt("Digite o índice de uma das suas tarefas realizadas")
+const indice = Number(indiceDeTarefas)
+listaDeTarefas.splice(indice,1)
+console.log(listaDeTarefas)*/
+
+
+/*let name = prompt("qual seu nome?")
+let email = prompt("qual seu E-mail?")
+console.log("o E-mail "+ email+ "foi cadastrado com sucesso")
+console.log("Seja bem vindo(a) ", name)*/
