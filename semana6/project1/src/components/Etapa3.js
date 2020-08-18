9e687b430f5bba0e11a210f0205297fa2d08cccd
Daniel Lemes <inventarartcomunicacao@hotmail.com>
2020-08-18 16:47:05 -0300

    exercícios semana6-aula22

---
+++ semana6/project1/src/components/Etapa3.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PerguntaFechadaEtapa3 from "./PerguntaFechadaEtapa3"
+
+
+class Etapa3 extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>ETAPA3 - INFORMAÇÕES GERAIS DE ENSINO</h1>
+            <PerguntaFechadaEtapa3 />
+               
+            </div>
+        )
+    }
+}
+export default Etapa3;
---
+++ semana6/project1/src/components/PerguntaAberta.js
@@ -0,0 +1,18 @@
+import React from "react"
+class PerguntaAberta extends React.Component {
+    render () {
+        return (
+            <div>
+                <h3>1. Qual o seu nome?</h3>
+                <input></input>
+                <h3>2. Qual sua idade?</h3>
+                <input ></input>
+                <h3>3. Qual seu email?</h3>
+                <input ></input>
+                
+            </div>
+
+        )
+    }
+}
+    export default PerguntaAberta;
