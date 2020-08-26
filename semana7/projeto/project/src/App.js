712fd2bfa994a4f1776f483efadb0f05e725e248
Daniel Lemes <inventarartcomunicacao@hotmail.com>
2020-08-26 17:10:06 -0300

    exercícios semana7-aula25-segundo dia

--- semana7/projeto/project/src/App.js
+++ semana7/projeto/project/src/App.js
@@ -1,108 +1,43 @@
 import React from "react";
-import axios from "axios";
+import styled from "styled-components";
+import UserListPage from "./components/userListPage";
+import CreateUserFormPage from "./components/createUserFormPage";
 
 
-export default class App extends React.Component {
+const AppContainer = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+`;
+
+class App extends React.Component {
   state = {
-  usuarios: [],
-    nomeUsuario: "",
-    emailUsuario:"",
-    id:""
+    currentPage: "createUserFormPage"
   };
 
-  pegarUsuarios() {
-    const request = axios.get(
-      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-      {
-        headers: {
-          Authorization: "daniel-lemes-jackson"
-        }
+  changePage = () => {
+    this.state.currentPage === "createUserFormPage"
+      ? this.setState({ currentPage: "userListPage" })
+      : this.setState({ currentPage: "createUserFormPage" });
+  };
+
+  render () {
+    const currentPage = () => {
+      if (this.state.currentPage === "createUserFormPage") {
+        return <CreateUserFormPage />;
+      } else if (this.state.currentPage === "userListPage") {
+        return <UserListPage />;
+      }else if(this.state.currentPage === "createUserId"){
+        return <userCreateId />
       }
-    );
-    request
-      .then((resposta) => {
-        this.setState({ usuarios: resposta.data });
-        console.log(resposta)
-      })
-      .catch((erro) => {
-        console.log("Ocorreu um erro");
-      });
-  }
-
-  criarUsuario = () => {
-    const body = {
-      name: this.state.nomeUsuario,
-      email: this.state.emailUsuario
     };
 
-    const request = axios.post(
-      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-      body,
-      {
-        headers: {
-          Authorization: "daniel-lemes-jackson"
-        }
-      }
-    );
-
-    request
-      .then((resposta) => {
-        alert(`o usúario ${this.state.nomeUsuario} E-mail ${this.state.emailUsuario} foi adicionado`);
-        this.pegarUsuarios();
-        this.setState({ nomeUsuario: "",emailUsuario:"" });
-      })
-      .catch((erro) => {
-        console.log("Erro");
-      });
-  }
-
-  deletarUsuario = (id) =>{
-    axios.delete( 
-     `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
-     {
-       headers: {
-          Authorization: "daniel-lemes-jackson"
-       }
-     } 
-     ).then((resposta) => {  
-      
-      alert(`o item ${resposta.id} foi excluido`)
-    })
-    .catch((erro) => {
-      console.log("Ocorreu um erro");
-    });
-  }
-  
-  
-  componentDidMount() {
-    this.pegarUsuarios();
-  }
-  render() {
-    const onChangeInput = (evento) => {
-      this.setState({ nomeUsuario: evento.target.value ,
-      });
-    };
-     const onChangeEmailInput = (evento) => {
-      this.setState({emailUsuario: evento.target.value ,
-      });
-    };
- 
     return (
-      <div>
-        <h1>Criar Usuarios</h1>
-        <input value={this.state.nomeUsuario} onChange={onChangeInput} placeholder="nome"/>
-        <input value={this.state.emailUsuario} onChange={onChangeEmailInput} placeholder="email"/>
-        <button onClick={() => this.criarUsuario(this.state.nomeUsuario)}>
-          Salvar
-        </button>
-        
-        {this.state.usuarios.map((item) => {
-          return <p key={item.id}>{item.name}
-           <button key={item.id} onClick={ () => this.deletarUsuario(item.id) }> Deletar </button>
-            
-          </p>     
-        })}
-      </div>
+      <AppContainer>
+        {currentPage()}
+        <button onClick={this.changePage}>Trocar de página</button>
+      </AppContainer>
     );
   }
 }
+
+export default App;
