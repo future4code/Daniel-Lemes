52dbb4d2eac7101bf0c6ea86c058c706321b76b6
Daniel Lemes <inventarartcomunicacao@hotmail.com>
2020-11-12 11:55:07 -0300

    projeto semana18 backend

---
+++ semana18/aula-4/intro-autenticacao_implementacao/src/data/insertTask.ts
@@ -0,0 +1,17 @@
+// import { connection } from "..";
+
+// export default async function insertTask(
+//     id: string,
+//     title: string,
+//     description: string,
+//  cratedAt:Date()
+// ) {
+//     await connection('Recipes')
+//         .insert({
+//             id,
+//             title,
+//             description,
+//             deadline,
+//             author_id: authorId
+//         })
+// }
